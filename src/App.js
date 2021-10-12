92d23868909844c39478217701d61a297c6d827b
poornesh-pH <poornesh9626@gmail.com>
2021-10-12 12:32:15 +0530

    3 files modified, 1 added, 1 deleted

--- src/App.js
+++ src/App.js
@@ -4,8 +4,7 @@ import ContactComponent from './ContactComponent';
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <h4>Users Dashboard</h4>
       <ContactComponent />
     </div>
   );
--- src/Store.js
+++ src/Store.js
@@ -1,5 +1,13 @@
-import React from 'react';
+import React, { createContext } from 'react';
 
 const Store = () => {};
 
+const initialState = {
+  name: '',
+  age: '',
+  email: '',
+  phone: '',
+};
+
+export const ContactContect = createContext(initialState);
 export default Store;
